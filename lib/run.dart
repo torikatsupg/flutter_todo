a19e361f06e2c0eecc35e03eb5c16752011f1132
torikatsu <torikatsu923@gmail.com>
2022-03-20 23:43:57 +0900

    connect to firebase

--- lib/run.dart
+++ lib/run.dart
@@ -1,8 +1,14 @@
 import 'package:flutter/widgets.dart';
 import 'package:flutter_todo/app.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void run() {
+void run() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(
     const ProviderScope(
       child: App(),
