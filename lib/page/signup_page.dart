b47dc557e615493b2b7214346c3209da1c039a63
torikatsu <torikatsu923@gmail.com>
2022-04-11 20:51:34 +0900

    remove unnecessary column

--- lib/page/signup_page.dart
+++ lib/page/signup_page.dart
@@ -21,35 +21,31 @@ class SignupPage extends ConsumerWidget {
             onPressed: () => context.go('/signin'),
             child: const Text('to signin'),
           ),
-          Column(
-            children: [
-              TextField(
-                controller: state.email.controller,
-                focusNode: state.email.focusNode,
-                textInputAction: TextInputAction.next,
-                decoration: InputDecoration(
-                  errorText: state.email.errors,
-                  hintText: 'xxxx@example.com',
-                  helperText: 'xxxx@example.com',
-                  label: const Text('email'),
-                ),
-              ),
-              TextField(
-                controller: state.password.controller,
-                focusNode: state.password.focusNode,
-                textInputAction: TextInputAction.done,
-                decoration: InputDecoration(
-                  errorText: state.password.errors,
-                  hintText: 'more than 8 characters',
-                  helperText: 'more than 8 characters',
-                  label: const Text('password'),
-                ),
-              ),
-              ElevatedButton(
-                onPressed: controller.submit,
-                child: const Text('submit'),
-              ),
-            ],
+          TextField(
+            controller: state.email.controller,
+            focusNode: state.email.focusNode,
+            textInputAction: TextInputAction.next,
+            decoration: InputDecoration(
+              errorText: state.email.errors,
+              hintText: 'xxxx@example.com',
+              helperText: 'xxxx@example.com',
+              label: const Text('email'),
+            ),
+          ),
+          TextField(
+            controller: state.password.controller,
+            focusNode: state.password.focusNode,
+            textInputAction: TextInputAction.done,
+            decoration: InputDecoration(
+              errorText: state.password.errors,
+              hintText: 'more than 8 characters',
+              helperText: 'more than 8 characters',
+              label: const Text('password'),
+            ),
+          ),
+          ElevatedButton(
+            onPressed: controller.submit,
+            child: const Text('submit'),
           ),
         ],
       ),
