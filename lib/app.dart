1c1bfed0795fd8606f30949cf3dca627be4dbabd
torikatsu <torikatsu923@gmail.com>
2022-04-17 19:14:28 +0900

    add initialize phase before mount RouterDelegate

--- lib/app.dart
+++ lib/app.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_todo/provider/route/go_router_provider.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
@@ -8,9 +9,26 @@ class App extends ConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final router = ref.watch(goRouterProvider);
-    return MaterialApp.router(
-      routeInformationParser: router.routeInformationParser,
-      routerDelegate: router.routerDelegate,
+
+    return FutureBuilder<bool>(
+      future: _init(ref),
+      builder: (context, snapshot) {
+        if (snapshot.hasData) {
+          return MaterialApp.router(
+            routeInformationParser: router.routeInformationParser,
+            routerDelegate: router.routerDelegate,
+          );
+        } else {
+          // TODO(torikatsu): show splash screen
+          return const Center(child: CircularProgressIndicator());
+        }
+      },
     );
   }
+}
+
+// initializing between runApp() and MaterialApp.router()
+Future<bool> _init(WidgetRef ref) async {
+  await FirebaseAuth.instance.authStateChanges().first;
+  return true;
 }
