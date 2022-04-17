517586f54f3d1e53a54287e42b48450311fe6fbb
torikatsu <torikatsu923@gmail.com>
2022-04-17 19:12:47 +0900

    modify auth provider

--- lib/firebase/auth_provider.dart
+++ lib/firebase/auth_provider.dart
@@ -1,16 +1,43 @@
+import 'dart:async';
+
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter_todo/model/app_error.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
-final authProvider = StateNotifierProvider<AuthStateNotifier, User?>((_) => AuthStateNotifier());
-
-class AuthStateNotifier extends StateNotifier<User?> {
-  AuthStateNotifier() : super(FirebaseAuth.instance.currentUser) {
-    FirebaseAuth.instance
-        .authStateChanges()
-        .listen((maybeUser) => state = maybeUser);
+final authProvider = Provider<User>((ref) {
+  final maybeUser = ref.watch(authStreamProvider).value;
+  if (maybeUser == null) {
+    throw AppError.unauthorized();
+  } else {
+    return maybeUser;
   }
+});
 
-  void setUser(User? user) {
-    state = user;
+final _prev = StateProvider<User?>((ref) => null);
+
+final authStreamProvider = StreamProvider<User?>((ref) {
+  final authStream = FirebaseAuth.instance.authStateChanges();
+  final stream = authStream.transform<User?>(
+    StreamTransformer.fromHandlers(
+      handleData: ((data, sink) {
+        if (data != ref.read(_prev)) {
+          ref.read(_prev.notifier).update((_) => data);
+          sink.add(data);
+        }
+      }),
+    ),
+  );
+  return stream;
+});
+
+extension StreamExt<T> on Stream<T> {
+  Future<T> next() {
+    final completer = Completer<T>();
+    final sub = listen(null);
+    sub.onData((e) {
+      sub.cancel();
+      completer.complete(e);
+    });
+    return completer.future;
   }
 }
