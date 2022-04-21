14a00ea0a713d38d20a0f2e984e5d081b1650924
torikatsu <torikatsu923@gmail.com>
2022-04-21 20:19:57 +0900

    fix bug showing todo list on done tab

--- lib/provider/controller/done_tab_controller.dart
+++ lib/provider/controller/done_tab_controller.dart
@@ -3,7 +3,8 @@ import 'package:flutter_todo/provider/infrastructure/auth_provider.dart';
 import 'package:flutter_todo/provider/model/task_provider.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
-final doneTabControllerProvider = FutureProvider.autoDispose<List<Task>>((ref) async {
+final doneTabControllerProvider =
+    FutureProvider.autoDispose<List<Task>>((ref) async {
   final uid = ref.watch(authProvider).uid;
-  return await ref.watch(todoTasksFamily(uid).future);
+  return await ref.watch(doneTasksFamily(uid).future);
 });
--- lib/view/page/task_tab/todo_tab.dart
+++ lib/view/page/task_tab/todo_tab.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_todo/provider/controller/task_controller_provider.dart';
 import 'package:flutter_todo/provider/controller/todo_tab_controller_provider.dart';
 import 'package:flutter_todo/view/component/error_view.dart';
 import 'package:flutter_todo/view/component/loading_view.dart';
@@ -18,21 +17,25 @@ class _TodoTabState extends ConsumerState {
   @override
   Widget build(context) {
     final state = ref.watch(todoTabControllerProvider);
-    final controller = ref.read(taskControllerProvider.notifier);
+    final controller = ref.read(todoTabControllerProvider.notifier);
     return state.map(
       data: (data) {
         final tasks = data.value;
-        return ListView.builder(
-          controller: _controller,
-          itemCount: tasks.length,
-          itemBuilder: (context, index) {
-            final task = tasks[index];
-            return ListTile(
-              leading: Text(task.id),
-              title: Text(task.name),
-              onTap: () => controller.toDetailPage(task.id),
-            );
-          },
+        return RefreshIndicator(
+          onRefresh: controller.onRefresh,
+          child: ListView.builder(
+            physics: const AlwaysScrollableScrollPhysics(),
+            controller: _controller,
+            itemCount: tasks.length,
+            itemBuilder: (context, index) {
+              final task = tasks[index];
+              return ListTile(
+                leading: Text(task.id),
+                title: Text(task.name),
+                onTap: () => controller.toDetailPage(task.id),
+              );
+            },
+          ),
         );
       },
       error: (_) => const ErrorView(),
