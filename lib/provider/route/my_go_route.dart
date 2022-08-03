75387c7e2734921d6fc852b226695c5abbc80f0e
torikatsu <torikatsu923@gmail.com>
2022-08-04 03:20:27 +0900

    override userProvider with localUserProvider

--- lib/provider/route/my_go_route.dart
+++ lib/provider/route/my_go_route.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_todo/provider/infrastructure/auth_provider.dart';
+import 'package:flutter_todo/provider/infrastructure/user_provider.dart';
 import 'package:flutter_todo/provider/local/local_auth_provider.dart';
 import 'package:flutter_todo/provider/route/router_provider.dart';
 import 'package:flutter_todo/view/component/loading_overlay.dart';
@@ -24,12 +24,15 @@ class MyGoRoute extends GoRoute {
           builder: (context, state) {
             assert(builder != null);
             final maybeAuth = read(authProvider).value;
+            final maybeUser = read(userProvider).value;
             final maybeTab = state.tab;
             final maybeTaskId = state.taskId;
             return ProviderScope(
               overrides: [
                 if (maybeAuth != null)
                   localAuthProvider.overrideWithValue(maybeAuth),
+                if (maybeUser != null)
+                  localUserProvider.overrideWithValue(maybeUser),
                 if (maybeTab != null)
                   localTabParamProvier.overrideWithValue(maybeTab),
                 if (maybeTaskId != null)
