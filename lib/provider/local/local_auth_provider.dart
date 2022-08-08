8d33aaf9203614c7a5e6a4a332fa92858d1ebcc0
torikatsu <torikatsu923@gmail.com>
2022-08-09 02:54:34 +0900

    fix local provider to use enum

--- lib/provider/local/local_auth_provider.dart
+++ lib/provider/local/local_auth_provider.dart
@@ -1,6 +1,7 @@
 import 'package:flutter_todo/model/task.dart';
 import 'package:flutter_todo/model/user.dart';
 import 'package:flutter_todo/model/user_auth.dart';
+import 'package:flutter_todo/provider/route/pram.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 final localAuthProvider = Provider<UserAuth>(
@@ -15,9 +16,9 @@ final localTaskIdParamProvier = Provider<TaskId>(
   (ref) => throw UnimplementedError(),
 );
 
-final localTabParamProvier = Provider<String>(
+final localTabParamProvier = Provider<HomeTab>(
   (ref) => throw UnimplementedError(),
 );
-final localTodoQueryParamProvier = Provider<String>(
+final localInnerTabProvier = Provider<InnerTab>(
   (ref) => throw UnimplementedError(),
 );
